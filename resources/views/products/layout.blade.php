6090515840c68ead854dd57e19c575347d7196c8
DevShaded <devshaded@gmail.com>
2020-05-20 18:50:08 +0200

    initial commit

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>CRUD Application</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <div class="container">
+       @yield('content')
+    </div>
+
+  </body>
+</html>
